74e9a3f8174c3e203ae5dfe9a23326cfd6cbae78
Willem Handreck <whreck1972@live.com>
2015-05-27 18:08:41 +1000

    Add node server

---
+++ static/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+app.get('*.*', function (req, res) {
+    res.sendFile(__dirname + '/static/' + req.path);
+});
+
+var port = process.env.PORT || 3000;
+server.listen(port, function () {
+    var host = server.address().address,
+        port = server.address().port;
+
+    console.log('App is listening at http://%s:%s', host, port);
+});
