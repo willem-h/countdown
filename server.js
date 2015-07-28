26155d36dd692fd559ff46789a0689402ed74b0c
Willem Handreck <whreck1972@live.com>
2015-07-28 17:16:16 +1000

    Change formatting to standard

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
-app.get('*.*', function (req, res) {
-    res.sendFile(__dirname + '/static/' + req.path);
+app.get('*.*', function(req, res) {
+  res.sendFile(__dirname + '/static/' + req.path);
 });
 
 var port = process.env.PORT || 3000;
-server.listen(port, function () {
-    var host = server.address().address,
-        port = server.address().port;
+server.listen(port, function() {
+  var host = server.address().address;
+  var port = server.address().port;
 
-    console.log('App is listening at http://%s:%s', host, port);
+  console.log('App is listening at http://%s:%s', host, port);
 });
